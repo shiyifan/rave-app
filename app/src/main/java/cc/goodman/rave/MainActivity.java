810b86601d898e071e9fad1494db5da95d227594
ivan <shiyifan00@live.cn>
2019-08-03 14:36:52 +0800

    create the navi bottom bar in main activity

--- app/src/main/java/cc/goodman/rave/MainActivity.java
+++ app/src/main/java/cc/goodman/rave/MainActivity.java
@@ -1,12 +1,14 @@
 package cc.goodman.rave;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
